dee28c2eafdf8eced01f4e38072a40fdeae4daf1
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 21:41:14 -0500

    Update app.py

--- app.py
+++ app.py
@@ -3,8 +3,7 @@ import streamlit as st
 from transformers import pipeline
 
 
-st.write('Enter text to be analysed')
-text = "After a good, but not great start in the franchise, McQuarrie creates the worst combination of the bad that came before him, with a mediocore script worse that MI-I, the below average action of MI-3, humor going over the top, and those unnecessary romantic touches that do not help the movie. I think there is one part of the movie, Vanessa Kirby's character and its plot, that is really questionable. Other than that, the movie is just an average action blockbuster."
+text = st.text_area('Enter text to be analysed')
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
 st.write(a)
