967bf518b06217831ca7f9f383039b8001dee55c
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-04 00:48:03 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,7 +7,7 @@ import matplotlib.pyplot as plt
 text = st.text_area('Enter text to be analysed')
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
-st.write(a)
+
 lab = a[0]['label']
 per = a[0]['score']
 opp_per = 1-a[0]['score']
