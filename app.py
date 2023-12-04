7d444b31e07be43fd92ed59dfe2d10be4dc3167e
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-04 00:41:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -24,7 +24,7 @@ elif(lab=='NEGATIVE'):
 colors = ["Green","Red"]
 fig1, ax1 = plt.subplots()
 ax1.pie(sizes, labels=labels,autopct='%1.1f%%',
-        shadow=True, startangle=90)
+        shadow=True, startangle=90, colors=colors)
 ax1.axis('equal')  # Equal aspect ratio ensures that pie is drawn as a circle.
 
 st.pyplot(fig1)
