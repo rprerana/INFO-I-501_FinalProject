b10c2464ce804f9447e6a8d3f7b4f8bbf3523afd
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-04 00:09:21 -0500

    Update app.py

--- app.py
+++ app.py
@@ -17,7 +17,7 @@ if(lab=='POSITIVE'):
   st.write('entered if ')
   labels = 'Positive', 'Negative'
   sizes = [per,opp_per]
-  explode = (0.5, 0)  # only "explode" the 2nd slice (i.e. 'Hogs')
+  explode = (0, 0.5)  # only "explode" the 2nd slice (i.e. 'Hogs')
 else:
   labels = 'Positive', 'Negative'
   sizes = [per,opp_per]
