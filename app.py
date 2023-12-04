e1e93e4934cf40e519292c6243c1bb0a51e600b2
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 20:49:07 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 
 
 st.write('Enter text to be analysed')
-text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+text = "After a good, but not great start in the franchise, McQuarrie creates the best combination of the good that came before him, with a better script that even MI-I, the unstoppable action of MI-3, good humor without going over the top contrary to MI-4, and those romantic touches that help the movie to slow down and feel a bit for the characters. I think there is one part of the movie, Vanessa Kirby's character and its plot, that is really questionable. Other than that, the movie is just a great all-around action blockbuster."
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
 st.write(a)
