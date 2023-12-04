0793a1b75f7692a579d8a849fc746c7fe1b8eb45
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 20:28:51 -0500

    Added app.py

---
+++ app.py
@@ -0,0 +1,8 @@
+import numpy as np
+import streamlit as st
+from transformers import pipeline
+
+text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
+classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
+a = classifier(text)
+st.write(a)
