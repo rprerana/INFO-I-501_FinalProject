97f31c686814cd445e4cffb2e047d5080c0e99d1
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 20:52:16 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,7 +4,7 @@ from transformers import pipeline
 
 
 st.write('Enter text to be analysed')
-text = "After a good, but not great start in the franchise, McQuarrie creates the best combination of the good that came before him, with a better script that even MI-I, the unstoppable action of MI-3, good humor without going over the top contrary to MI-4, and those romantic touches that help the movie to slow down and feel a bit for the characters. I think there is one part of the movie, Vanessa Kirby's character and its plot, that is really questionable. Other than that, the movie is just a great all-around action blockbuster."
+text = "After a good, but not great start in the franchise, McQuarrie creates the worst combination of the bad that came before him, with a mediocore script worse that MI-I, the below average action of MI-3, humor going over the top, and those unnecessary romantic touches that do not help the movie. I think there is one part of the movie, Vanessa Kirby's character and its plot, that is really questionable. Other than that, the movie is just an average action blockbuster."
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
 st.write(a)
