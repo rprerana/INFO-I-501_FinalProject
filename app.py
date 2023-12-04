6e472f6c5d5e2ddf70924db013b9f9a8d323bb23
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 23:03:24 -0500

    Update app.py

--- app.py
+++ app.py
@@ -7,5 +7,8 @@ text = st.text_area('Enter text to be analysed')
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
 st.write(a)
-st.write(a[0]['label'])
-st.write(a[0]['score'])
+lab = a[0]['label']
+per = a[0]['score']
+opp_per = 1-a[0]['score']
+st.write(opp_per)
+st.write(per)
