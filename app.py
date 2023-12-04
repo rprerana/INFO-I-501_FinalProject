c6f9088f3fc416acf43459f8ed59812200515ffe
Prerana <43247643+rprerana@users.noreply.github.com>
2023-12-03 20:39:13 -0500

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,8 @@ import numpy as np
 import streamlit as st
 from transformers import pipeline
 
+
+st.write('Enter text to be analysed')
 text = "This was a masterpiece. Not completely faithful to the books, but enthralling from beginning to end. Might be my favorite of the three."
 classifier = pipeline("sentiment-analysis", model="preranar/my_awesome_model")
 a = classifier(text)
